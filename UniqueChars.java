a6ffab82937e9e71d93410037f7417ea4daa6ea4
Arad <arad.koluok@post.runi.ac.il>
2024-01-07 20:05:17 +0200

    what works, works

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,7 +11,35 @@ public class UniqueChars {
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+        String oldStr = s;
+        String newStr = "";
+        char ch;
+        int doesCharExistsNewStr;
+        
+        
+        for (int i = 0; i< oldStr.length() - 1; i++) {
+            ch = oldStr.charAt(i);
+            doesCharExistsNewStr = newStr.indexOf(ch);
+            if (doesCharExistsNewStr < 0 || ch == ' ') {
+                newStr += ch;
+            }
+        }
+        return newStr;
+    }
+
+    public static boolean doesCharMoreThanOnceInStr(String text, char chr) {
+        int repeatCharCounter = 0, loopCounter = 0;
+        int stringLength = text.length();
+        while (repeatCharCounter <= 1 && loopCounter <= stringLength -1) {
+            if(text.charAt(loopCounter) == chr) {
+                repeatCharCounter++;
+            }
+            loopCounter++;
+        }
+
+        if(repeatCharCounter == 2) {
+            return true;
+        }
+        return false;
     }
 }
